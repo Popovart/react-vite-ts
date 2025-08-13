866fc85c9397f7595afdc2795c2a98554a5298ab
dmitrypopov <Mintriy2033@yandex.ru>
2025-08-13 14:26:48 +0200

    tailwind and prettier setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    viteTsconfigPaths() // to avoid aliases duplication 
+    viteTsconfigPaths(), // to avoid aliases duplication
+    tailwindcss()
   ],
 })
