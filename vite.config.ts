4682296514949a4ab6c1e091350f6c53d6eef60f
dmitrypopov <Mintriy2033@yandex.ru>
2025-08-14 19:35:14 +0200

    install and configure storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite'
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import viteTsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite"
+
+//storybook imports
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin"
+const dirname =
+    typeof __dirname !== "undefined"
+        ? __dirname
+        : path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteTsconfigPaths({ projects: ['tsconfig.app.json'] }), // to avoid aliases duplication
-    tailwindcss()
-  ],
+    plugins: [
+        react(),
+        viteTsconfigPaths({ projects: ["tsconfig.app.json"] }),
+        tailwindcss(),
+    ],
+    test: {
+        projects: [
+            {
+                extends: true,
+                plugins: [
+                    storybookTest({
+                        configDir: path.join(dirname, ".storybook"),
+                    }),
+                ],
+                test: {
+                    name: "storybook",
+                    browser: {
+                        enabled: true,
+                        headless: true,
+                        provider: "playwright",
+                        instances: [
+                            {
+                                browser: "chromium",
+                            },
+                        ],
+                    },
+                    setupFiles: [".storybook/vitest.setup.ts"],
+                },
+            },
+        ],
+    },
 })
