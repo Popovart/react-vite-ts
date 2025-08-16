ee67c021cd5927a70c28169d4dee917178723112
dmitrypopov <Mintriy2033@yandex.ru>
2025-08-16 22:26:09 +0200

    format component code

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -1,27 +1,27 @@
 import { cn } from "@/utils/cn"
 
 const sizes = {
-    sm: "h-4 w-4",
-    md: "h-8 w-8",
-    lg: "h-16 w-16",
-    xl: "h-24 w-24",
+    sm: 'h-4 w-4',
+    md: 'h-8 w-8',
+    lg: 'h-16 w-16',
+    xl: 'h-24 w-24'
 }
 
 const variants = {
-    light: "text-white",
-    primary: "text-slate-600",
+    light: 'text-white',
+    primary: 'text-state-600'
 }
 
 export type SpinnerProps = {
-    size?: keyof typeof sizes
-    variant?: keyof typeof variants
-    className?: string
+    size?: keyof typeof sizes;
+    variant?: keyof typeof variants;
+    className?: string;
 }
 
 export const Spinner = ({
-    size = "md",
-    variant = "primary",
-    className = "",
+    size = 'md',
+    variant = 'primary',
+    className = ''
 }: SpinnerProps) => {
     return (
         <>
@@ -36,15 +36,15 @@ export const Spinner = ({
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 className={cn(
-                    "animate-spin",
+                    'animate-spin',
                     sizes[size],
                     variants[variant],
-                    className
+                    className,
                 )}
             >
                 <path d="M21 12a9 9 0 1 1-6.219-8.56" />
             </svg>
-            <div className="sr-only">Loading</div>
+            <span className="sr-only">Loading</span>
         </>
     )
 }
