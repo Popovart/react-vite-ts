4f77b9d327c910f50cbe1e3439efaad1a4f21a0e
dmitrypopov <Mintriy2033@yandex.ru>
2025-09-15 15:49:03 +0200

    Add notifications store with Zustand

---
+++ src/components/ui/notifications/notification-store.ts
@@ -0,0 +1,29 @@
+import { nanoid } from "nanoid"
+import { create } from "zustand"
+import { type Notification as NotificationData } from "./types"
+
+type NotificationsStore = {
+    notifications: NotificationData[]
+    addNotification: (notification: Omit<NotificationData, "id">) => void
+    dismissNotification: (id: string) => void
+}
+
+export const useNotifications = create<NotificationsStore>((set) => ({
+    notifications: [],
+    addNotification: (notification) => 
+        set((state) => ({
+            notifications: [
+                ...state.notifications,
+                {
+                    id: nanoid(),
+                    ...notification
+                }
+            ]
+        })),
+    dismissNotification: (id) =>
+        set((state) => ({
+            notifications: state.notifications.filter(
+                (notification) => notification.id !== id
+            )
+        }))
+}));
